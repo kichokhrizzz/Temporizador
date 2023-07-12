901160fe7937408d4df06498591c15dcfdda59c1
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-07-12 09:35:36 -0600

    eleven commit

--- Temporizador/ContentView.swift
+++ Temporizador/ContentView.swift
@@ -116,114 +116,35 @@ struct ContentView: View {
                 updateCountdown()
             }
             .navigationBarItems(trailing:
-                                    Button(action: {
-                isMenuOpen.toggle()
-            }) {
-                Image(systemName: "line.3.horizontal")
-                    .imageScale(.large)
-                    .foregroundColor(.orange)
-            }
+                                    Menu {
+                                        Button(action: {
+                                            showSettings = true
+                                        }) {
+                                            Label("Configuración", systemImage: "gear")
+                                        }
+                                        
+                                        Button(action: {
+                                            showComments = true
+                                        }) {
+                                            Label("Comentarios", systemImage: "newspaper.fill")
+                                        }
+                                    } label: {
+                                        Image(systemName: "line.3.horizontal")
+                                            .imageScale(.large)
+                                            .foregroundColor(.orange)
+                                    }
             )
-            .fullScreenCover(isPresented: $isMenuOpen) {
-                NavigationView {
-                    VStack {
-                        Form {
-                            Section(header: Text("Configuración")) {
-                                Toggle(isOn: $vibrateAndSound.animation()) {
-                                    Text("Vibrar y Sonar")
-                                }
-                                .onChange(of: vibrateAndSound) { newValue in
-                                    if newValue {
-                                        vibrateOnly = false
-                                        soundOnly = false
-                                    }
-                                }
-                                
-                                Toggle(isOn: $vibrateOnly.animation()) {
-                                    Text("Solo Vibrar")
-                                }
-                                .onChange(of: vibrateOnly) { newValue in
-                                    if newValue {
-                                        vibrateAndSound = false
-                                        soundOnly = false
-                                    } else if !newValue && !soundOnly {
-                                        vibrateAndSound = true
-                                    }
-                                }
-                                
-                                Toggle(isOn: $soundOnly.animation()) {
-                                    Text("Solo Sonar")
-                                }
-                                .onChange(of: soundOnly) { newValue in
-                                    if newValue {
-                                        vibrateAndSound = false
-                                        vibrateOnly = false
-                                    } else if !newValue && !vibrateOnly {
-                                        vibrateAndSound = true
-                                    }
-                                }
-                            }
-                            
-                            Section(header: Text("Tipo de sonido")) {
-                                HStack {
-                                    Text("Sonido predeterminado")
-                                    Spacer()
-                                    if selectedSound == .defaultSound {
-                                        Image(systemName: "checkmark")
-                                    }
-                                }
-                                .onTapGesture {
-                                    selectedSound = .defaultSound
-                                }
-                                
-                                HStack {
-                                    Text("Sonido alternativo")
-                                    Spacer()
-                                    if selectedSound == .alternativeSound {
-                                        Image(systemName: "checkmark")
-                                    }
-                                }
-                                .onTapGesture {
-                                    selectedSound = .alternativeSound
-                                }
-                            }
-                            
-                            Section(header: Text("Tipo de vibración")) {
-                                HStack {
-                                    Text("Vibración predeterminada")
-                                    Spacer()
-                                    if selectedVibration == .defaultVibration {
-                                        Image(systemName: "checkmark")
-                                    }
-                                }
-                                .onTapGesture {
-                                    selectedVibration = .defaultVibration
-                                }
-                                
-                                HStack {
-                                    Text("Vibración intensa")
-                                    Spacer()
-                                    if selectedVibration == .heavyVibration {
-                                        Image(systemName: "checkmark")
-                                    }
-                                }
-                                .onTapGesture {
-                                    selectedVibration = .heavyVibration
-                                }
-                            }
-                        }
-                        
-                        Spacer()
-                    }
-                    .navigationBarTitle("Configuración")
-                    .navigationBarItems(trailing:
-                                            Button(action: {
-                        isMenuOpen = false
-                    }) {
-                        Text("Cerrar")
-                    }
-                    )
-                }
+            .sheet(isPresented: $showSettings) {
+                SettingsView(
+                    vibrateAndSound: $vibrateAndSound,
+                    vibrateOnly: $vibrateOnly,
+                    soundOnly: $soundOnly,
+                    selectedSound: $selectedSound,
+                    selectedVibration: $selectedVibration
+                )
+            }
+            .sheet(isPresented: $showComments) {
+                CommentsView()
             }
         }
         .onChange(of: countdown) { newValue in
@@ -241,7 +162,7 @@ struct ContentView: View {
                 countdown -= 1
             }
         }
-        showMessage = false // Eliminar el mensaje de "Esperando para iniciar"
+        showMessage = false
     }
     
     private func stopCountdown() {
@@ -315,7 +236,6 @@ struct ContentView: View {
             case .defaultVibration:
                 AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
             case .heavyVibration:
-                // Reproducir la vibración intensa por un período de tiempo más largo
                 let durationInSeconds: TimeInterval = 2.0 // Duración de la vibración en segundos
                 let endTime = Date().addingTimeInterval(durationInSeconds)
                 
@@ -325,6 +245,109 @@ struct ContentView: View {
                 }
             }
         }
+    }
+}
+
+struct SettingsView: View {
+    @Binding var vibrateAndSound: Bool
+    @Binding var vibrateOnly: Bool
+    @Binding var soundOnly: Bool
+    @Binding var selectedSound: ContentView.SoundType
+    @Binding var selectedVibration: ContentView.VibrationType
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Configuración")) {
+                Toggle(isOn: $vibrateAndSound.animation()) {
+                    Text("Vibrar y Sonar")
+                }
+                .onChange(of: vibrateAndSound) { newValue in
+                    if newValue {
+                        vibrateOnly = false
+                        soundOnly = false
+                    }
+                }
+                
+                Toggle(isOn: $vibrateOnly.animation()) {
+                    Text("Solo Vibrar")
+                }
+                .onChange(of: vibrateOnly) { newValue in
+                    if newValue {
+                        vibrateAndSound = false
+                        soundOnly = false
+                    } else if !newValue && !soundOnly {
+                        vibrateAndSound = true
+                    }
+                }
+                
+                Toggle(isOn: $soundOnly.animation()) {
+                    Text("Solo Sonar")
+                }
+                .onChange(of: soundOnly) { newValue in
+                    if newValue {
+                        vibrateAndSound = false
+                        vibrateOnly = false
+                    } else if !newValue && !vibrateOnly {
+                        vibrateAndSound = true
+                    }
+                }
+            }
+            
+            Section(header: Text("Tipo de sonido")) {
+                HStack {
+                    Text("Sonido predeterminado")
+                    Spacer()
+                    if selectedSound == .defaultSound {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                .onTapGesture {
+                    selectedSound = .defaultSound
+                }
+                
+                HStack {
+                    Text("Sonido alternativo")
+                    Spacer()
+                    if selectedSound == .alternativeSound {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                .onTapGesture {
+                    selectedSound = .alternativeSound
+                }
+            }
+            
+            Section(header: Text("Tipo de vibración")) {
+                HStack {
+                    Text("Vibración predeterminada")
+                    Spacer()
+                    if selectedVibration == .defaultVibration {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                .onTapGesture {
+                    selectedVibration = .defaultVibration
+                }
+                
+                HStack {
+                    Text("Vibración intensa")
+                    Spacer()
+                    if selectedVibration == .heavyVibration {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                .onTapGesture {
+                    selectedVibration = .heavyVibration
+                }
+            }
+        }
+        .navigationBarTitle("Configuración")
+    }
+}
+
+struct CommentsView: View {
+    var body: some View {
+        Text("Pantalla de comentarios")
     }
 }
 
