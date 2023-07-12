28d1f1deef57c5ce147c75be23abcdb00ef24a5c
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-07-11 22:22:31 -0600

    first commit

--- Temporizador/ContentView.swift
+++ Temporizador/ContentView.swift
@@ -8,16 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countdown: TimeInterval = 10 * 60 // 10 minutos en segundos
+    @State private var isCountingDown = false
+    @State private var showMessage = false
+    @State private var timer: Timer?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Spacer()
+                    
+                    Text(timeFormatted(countdown))
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                    if showMessage {
+                        Text("Configurada. Esperando para iniciar")
+                            .foregroundColor(Color.white)
+                            .font(.headline)
+                            .padding()
+                    }
+                    
+                    Button(action: {
+                        if isCountingDown {
+                            stopCountdown()
+                        } else {
+                            startCountdown()
+                            showMessage = true
+                        }
+                    }) {
+                        Text(isCountingDown ? "Cancelar" : "Botón")
+                            .font(.headline)
+                            .padding()
+                            .background(isCountingDown ? Color.red : Color("orange"))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                    .onChange(of: isCountingDown) { newValue in
+                        if !newValue {
+                            showMessage = false
+                            updateCountdown()
+                        }
+                    }
+                    
+                    Spacer()
+                }
+            }
+            .onAppear {
+                updateCountdown()
+            }
+            .navigationBarItems(trailing:
+                Button(action: {
+                    // Acción del botón
+                }) {
+                    Image(systemName: "line.3.horizontal")
+                }
+            )
         }
-        .padding()
+    }
+    
+    private func startCountdown() {
+        isCountingDown = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            countdown -= 1
+            if countdown <= 0 {
+                stopCountdown()
+            }
+        }
+    }
+    
+    private func stopCountdown() {
+        isCountingDown = false
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func updateCountdown() {
+        countdown = 10 * 60
+    }
+    
+    private func timeFormatted(_ totalSeconds: TimeInterval) -> String {
+        let minutes = Int(totalSeconds) / 60
+        let seconds = Int(totalSeconds) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
